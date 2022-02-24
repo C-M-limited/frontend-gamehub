4b2e1d656efa9a666f3d6dfa3bcee74fa3aa8c89
marttsang2 <=>
2022-02-24 17:32:53 +0800

    update auth

--- store/action/auth.ts
+++ store/action/auth.ts
@@ -7,39 +7,39 @@ export const LOG_OUT_REQUEST = "LOG_OUT_REQUEST"
 export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS"
 export const LOG_OUT_FAILED = "LOG_OUT_FAILED"
 
-export const logInRequest =() =>{
-    return{
+export const logInRequest = () => {
+    return {
         type: LOG_IN_REQUEST
     }
 }
 
-export const logInSuccess =() =>{
-    return{
+export const logInSuccess = () => {
+    return {
         type: LOG_IN_SUCCESS
     }
 }
 
-export const logInFailed = (errMsg: string) =>{
-    return{
+export const logInFailed = (errMsg: string) => {
+    return {
         type: LOG_IN_FAILED,
         payload: errMsg
     }
 }
 
-export const logOutRequest =() =>{
-    return{
+export const logOutRequest = () => {
+    return {
         type: LOG_OUT_REQUEST
     }
 }
 
-export const logOutSuccess =() =>{
-    return{
+export const logOutSuccess = () => {
+    return {
         type: LOG_OUT_SUCCESS
     }
 }
 
-export const logOutFailed = (errMsg: string) =>{
-    return{
+export const logOutFailed = (errMsg: string) => {
+    return {
         type: LOG_OUT_FAILED,
         payload: errMsg
     }
@@ -50,31 +50,31 @@ interface logInProps {
     password: string;
 }
 
-export const login = ({ email, password}: logInProps) => {
+export const login = ({ email, password }: logInProps) => {
     return async (dispatch: any) => {
         dispatch(logInRequest())
-        
-        axios.post(`${server}/api/v1/login`, {
+
+        axios.post(`/api/v1/login`, {
             email,
             password
-          })
-          .then(function (response){
-            if (response.data==="This account is already LogIn"){
-              dispatch(logInFailed(response.data))
-              alert('Login Failed')
-              return;
-            }
-            localStorage.setItem('access-token',response.data.AUTHORIZATION);
-            localStorage.setItem('refresh-token',response.data.refreshToken);
-            dispatch(logInSuccess());
-          })
-          .catch(function (error){
-            dispatch(logInFailed("Login Failed"));
-            alert('Login Failed')
-            console.log(error.response)
-          })
-         }
+        })
+            .then(function (response) {
+                if (response.data === "This account is already LogIn") {
+                    dispatch(logInFailed(response.data))
+                    alert('Login Failed')
+                    return;
+                }
+                localStorage.setItem('access-token', response.data.AUTHORIZATION);
+                localStorage.setItem('refresh-token', response.data.refreshToken);
+                dispatch(logInSuccess());
+            })
+            .catch(function (error) {
+                dispatch(logInFailed("Login Failed"));
+                alert('Login Failed')
+                console.log(error.response)
+            })
     }
+}
 
 export const logOut = () => {
     return async (dispatch: any) => {
@@ -84,6 +84,6 @@ export const logOut = () => {
 
 export const register = () => {
     return async (dispatch: any) => {
-        
+
     }
 }
