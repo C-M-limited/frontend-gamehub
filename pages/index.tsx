31530f9a071c10299f5acc55dfa69264319153f2
Chris <chrisleebed@gmail.com>
2022-02-27 21:48:39 +0800

    add todat's pick on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ interface postDetailProps{
   game_name: string;
   image_url: string;
 }
-const Home: NextPage = ({latestGames}:any) => {
+const Home: NextPage = ({latestGames,todaysGames}:any) => {
   // console.log(latestGames.gameDetails)
   return (
     <>
@@ -40,7 +40,17 @@ const Home: NextPage = ({latestGames}:any) => {
       <Grid container display="flex" justifyContent="center">
         <Grid item lg={10}>
           <SubTitle>Today{"'"}s Picks</SubTitle>
-          <TodaysPickGameItem name="GTA-5" image_src="/game_sample.png" price={200} location="Yau Tong"/>
+          <Grid container spacing={2} display={"flex"} justifyContent="center">
+          {todaysGames.map((game:gameProps)=>{
+            const { game_sale_post,user_name,game_name,image_url } = game;
+            const {id,price,place_for_transaction,created_date,description,contact_method,user_Id,games_ID } = game_sale_post
+            return(
+              <Grid item key={id}>
+              <TodaysPickGameItem name={game_name} image_src={image_url} price={price} location={place_for_transaction}/>
+              </Grid>
+            )
+          })}
+          </Grid>
         </Grid>
       </Grid>
       {/* Latest Post */}
@@ -70,11 +80,14 @@ export default Home
 export const getServerSideProps = async () => {
   // const { gameId } = context.query
 
-  const res = await (await axios.get(`${server}/api/v1/game_sale_post/latestPost`))
-  const latestGames = res.data.content;
+  const latestRes = await (await axios.get(`${server}/api/v1/game_sale_post/latestPost`))
+  const todaysRes = await (await axios.get(`${server}/api/v1/post_click_rate`))
+  const latestGames = latestRes.data.content;
+  const todaysGames = todaysRes.data.content;
   return {
       props: {
-            latestGames
+            latestGames,
+            todaysGames
       }
   }
 }
