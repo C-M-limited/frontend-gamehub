a9b03fda2d9e78743580bde6fb3ba89f7a8d973f
marttsang2 <=>
2022-03-04 15:04:03 +0800

    update index page error handling

--- config/index.tsx
+++ config/index.tsx
@@ -1,3 +1,11 @@
+import axios from "axios"
+
 const dev = process.env.NODE_ENV !== 'production'
 
 export const server = dev ? 'http://localhost:8080' : 'https://yourwebsite.com'
+
+export const axiosInstance = axios.create({
+    baseURL: `${server}/api/v1/`,
+    timeout: 1000,
+    headers: {'X-Custom-Header': 'foobar'},
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,9 @@ import axios from 'axios'
 import type { NextPage } from 'next'
 import StyledHero from '../components/template/StyledHero'
 import TodaysPickGameItem from '../components/template/TodaysPickGameItem'
-import { server } from '../config'
+import { axiosInstance, server } from '../config'
 import Carousel from 'react-material-ui-carousel'
-import { Paper, Button ,Box} from '@mui/material'
+import { Paper, Button, Box } from '@mui/material'
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 const playstation = require('../public/game_list/playstation.json')
@@ -19,23 +19,23 @@ const SubTitle = styled(Typography)({
   background: 'linear-gradient(90deg, rgba(102,0,255,1) 0%, rgba(180,28,255,1) 100%)',
   borderRadius: 4,
   padding: '8px 20px',
-  display:'flex',
-  justifyContent:'center',
-  alignItems:'center',
-  width:'200px',
-  marginBottom:'30px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '200px',
+  marginBottom: '30px',
   border: '2px solid gold'
 })
 
 
-interface gameProps{
+interface gameProps {
   game_sale_post: postDetailProps;
   user_name: string;
   game_name: string;
   image_url: string;
   console_brand_name: string;
 }
-interface postDetailProps{
+interface postDetailProps {
   id: number;
   price: number,
   place_for_transaction: string;
@@ -45,74 +45,75 @@ interface postDetailProps{
   user_Id: number;
   games_ID: number;
 }
-const Home: NextPage = ({latestGames,todaysGames}:any) => {
+const Home: NextPage = ({ latestGames, todaysGames }: any) => {
   // console.log(latestGames.gameDetails)
   return (
     <>
-      <StyledHero/>
-      {/* todays Pick */}
-      <Grid container display="flex" justifyContent="center">
-        <Grid item lg={10}>
+      <StyledHero />
+      {
+        todaysGames.length > 0
+        &&
+        <Grid container display="flex" justifyContent="center">
+          <Grid item lg={10}>
             <SubTitle>Today{"'"}s Picks</SubTitle>
             {/* show 5 on xl */}
-            <Grid container spacing={2} sx={{ display: { xs: 'none', sm:'none',md:'flex',lg:'flex', xl: 'flex' } }} justifyContent="center" >
-            {todaysGames.map((game:gameProps)=>{
-              const { game_sale_post,user_name,game_name,image_url,console_brand_name } = game;
-              const {id,price,place_for_transaction,created_date,description,contact_method,user_Id,games_ID } = game_sale_post
-              return(
-                <Grid item key={id}>
+            <Grid container spacing={2} sx={{ display: { xs: 'none', sm: 'none', md: 'flex', lg: 'flex', xl: 'flex' } }} justifyContent="center" >
+              {todaysGames.map((game: gameProps) => {
+                const { game_sale_post, user_name, game_name, image_url, console_brand_name } = game;
+                const { id, price, place_for_transaction, created_date, description, contact_method, user_Id, games_ID } = game_sale_post
+                return (
+                  <Grid item key={id}>
 
-                  <TodaysPickGameItem game_id={id} name={game_name} image_src={image_url} price={price} location={place_for_transaction} brand={console_brand_name}/>
+                    <TodaysPickGameItem game_id={id} name={game_name} image_src={image_url} price={price} location={place_for_transaction} brand={console_brand_name} />
 
-                </Grid> 
-              )
-            })}
+                  </Grid>
+                )
+              })}
             </Grid>
             {/* show carousel on small screen */}
-            <Box sx={{ display: { xs: 'block',sm:'block',md:'none',lg:'none', xl: 'none' } }}>
-              <Carousel     NextIcon={<KeyboardArrowRightIcon/>} PrevIcon={<KeyboardArrowLeftIcon/>}>
-              {todaysGames.map((game:gameProps)=>{
-                const { game_sale_post,user_name,game_name,image_url,console_brand_name } = game;
-                const {id,price,place_for_transaction,created_date,description,contact_method,user_Id,games_ID } = game_sale_post
-                return(
-                    <TodaysPickGameItem key={id} game_id={id} name={game_name} image_src={image_url} price={price} location={place_for_transaction} brand={console_brand_name}/>
-                )
-              })}
+            <Box sx={{ display: { xs: 'block', sm: 'block', md: 'none', lg: 'none', xl: 'none' } }}>
+              <Carousel NextIcon={<KeyboardArrowRightIcon />} PrevIcon={<KeyboardArrowLeftIcon />}>
+                {todaysGames.map((game: gameProps) => {
+                  const { game_sale_post, user_name, game_name, image_url, console_brand_name } = game;
+                  const { id, price, place_for_transaction, created_date, description, contact_method, user_Id, games_ID } = game_sale_post
+                  return (
+                    <TodaysPickGameItem key={id} game_id={id} name={game_name} image_src={image_url} price={price} location={place_for_transaction} brand={console_brand_name} />
+                  )
+                })}
               </Carousel>
             </Box>
-
-        </Grid>
-      </Grid>
-      {/* Latest Post */}
-      <Grid container display="flex" justifyContent="center" mt={5}>
-        <Grid item lg={10}>
-          <SubTitle>Latest Posts</SubTitle>
-          <Grid container spacing={2} sx={{ display: { xs: 'none', sm:'none',md:'flex',lg:'flex', xl: 'flex' } }} justifyContent="center">
-          {latestGames.map((game:gameProps)=>{
-            const { game_sale_post,user_name,game_name,image_url,console_brand_name } = game;
-            const {id,price,place_for_transaction,created_date,description,contact_method,user_Id,games_ID } = game_sale_post
-            return(
-              <Grid item key={id}>
-              <TodaysPickGameItem game_id={id} name={game_name} image_src={image_url} price={price} location={place_for_transaction} brand={console_brand_name}/>
-              </Grid>
-            )
-          })}
           </Grid>
-          {/* show carousel on small screen */}
-          <Box sx={{ display: { xs: 'block',sm:'block',md:'none',lg:'none', xl: 'none' } }}>
-            <Carousel     NextIcon={<KeyboardArrowRightIcon/>} PrevIcon={<KeyboardArrowLeftIcon/>}>
-              {latestGames.map((game:gameProps)=>{
-                const { game_sale_post,user_name,game_name,image_url,console_brand_name } = game;
-                const {id,price,place_for_transaction,created_date,description,contact_method,user_Id,games_ID } = game_sale_post
-                return(
-                  <TodaysPickGameItem  key={id} game_id={id} name={game_name} image_src={image_url} price={price} location={place_for_transaction} brand={console_brand_name}/>
+        </Grid>}
+      {latestGames.length > 0
+        &&
+        <Grid container display="flex" justifyContent="center" mt={5}>
+          <Grid item lg={10}>
+            <SubTitle>Latest Posts</SubTitle>
+            <Grid container spacing={2} sx={{ display: { xs: 'none', sm: 'none', md: 'flex', lg: 'flex', xl: 'flex' } }} justifyContent="center">
+              {latestGames.map((game: gameProps) => {
+                const { game_sale_post, user_name, game_name, image_url, console_brand_name } = game;
+                const { id, price, place_for_transaction, created_date, description, contact_method, user_Id, games_ID } = game_sale_post
+                return (
+                  <Grid item key={id}>
+                    <TodaysPickGameItem game_id={id} name={game_name} image_src={image_url} price={price} location={place_for_transaction} brand={console_brand_name} />
+                  </Grid>
                 )
               })}
-            </Carousel>
-          </Box>
-
-        </Grid>
-      </Grid>
+            </Grid>
+            {/* show carousel on small screen */}
+            <Box sx={{ display: { xs: 'block', sm: 'block', md: 'none', lg: 'none', xl: 'none' } }}>
+              <Carousel NextIcon={<KeyboardArrowRightIcon />} PrevIcon={<KeyboardArrowLeftIcon />}>
+                {latestGames.map((game: gameProps) => {
+                  const { game_sale_post, user_name, game_name, image_url, console_brand_name } = game;
+                  const { id, price, place_for_transaction, created_date, description, contact_method, user_Id, games_ID } = game_sale_post
+                  return (
+                    <TodaysPickGameItem key={id} game_id={id} name={game_name} image_src={image_url} price={price} location={place_for_transaction} brand={console_brand_name} />
+                  )
+                })}
+              </Carousel>
+            </Box>
+          </Grid>
+        </Grid>}
     </>
   )
 }
@@ -120,16 +121,22 @@ const Home: NextPage = ({latestGames,todaysGames}:any) => {
 export default Home
 
 export const getServerSideProps = async () => {
-  // const { gameId } = context.query
+  let latestGames = []
+  let todaysGames = []
 
-  const latestRes = await (await axios.get(`${server}/api/v1/game_sale_post/latestPost`))
-  const todaysRes = await (await axios.get(`${server}/api/v1/post_click_rate`))
-  const latestGames = latestRes.data.content;
-  const todaysGames = todaysRes.data.content;
+  try {
+    const latestRes = await axiosInstance.get('game_sale_post/latestPost')
+    const todaysRes = await axios.get('post_click_rate')
+    latestGames = latestRes.data.content;
+    todaysGames = todaysRes.data.content;
+  }
+  catch (err) {
+  }
+
   return {
-      props: {
-            latestGames,
-            todaysGames
-      }
+    props: {
+      latestGames,
+      todaysGames
+    }
   }
 }
