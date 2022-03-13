5c822bc6c1e88989b70a5d11e914d464796fe625
Martin Tsang <martin.tsang.technine@gmail.com>
2022-03-13 20:28:20 +0800

    update config setting

--- config/index.tsx
+++ config/index.tsx
@@ -2,10 +2,13 @@ import axios from "axios"
 
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:8080' : 'https://app.gamehub.link'
+export const server = dev ? 'https://app.gamehub.link' : 'https://app.gamehub.link'
 
 export const axiosInstance = axios.create({
     baseURL: `${server}/api/v1/`,
     timeout: 1000,
-    headers: {'X-Custom-Header': 'foobar'},
+    headers: {
+        'X-Custom-Header': 'foobar',
+        'Access-Control-Allow-Origin': '*',
+    },
 })
