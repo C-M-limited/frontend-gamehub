ef96d023412235eeb842a174cc43f437c5a513e7
Chris <chrisleebed@gmail.com>
2022-03-19 18:38:48 +0800

    Change the ui of styled alert

--- components/alerts/StyledAlert.tsx
+++ components/alerts/StyledAlert.tsx
@@ -18,7 +18,7 @@ export default function StyledAlert() {
     }
   return (
     <Snackbar open={isOpenAlert.isOpen} autoHideDuration={4000} onClose={handleClose}>
-      <Box sx={{width:'200px', position:'fixed', zIndex:'100', left:'50%' ,marginLeft:'-100px', top:'10%'}}>
+      <Box sx={{width:'300px', position:'fixed', zIndex:'100', left:'50%' ,marginLeft:'-150px', top:'10%'}}>
         <Alert  variant="filled"  severity={isOpenAlert.type}>{isOpenAlert.content}</Alert>
       </Box>
       </Snackbar>
