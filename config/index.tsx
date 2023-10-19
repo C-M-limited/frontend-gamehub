5737c7be78cbeb78381bd2f489720a6c293ac1c3
Martin Tsang <tsangmartin1225@gmail.com>
2023-10-19 13:44:13 -0400

    update config

--- config/index.tsx
+++ config/index.tsx
@@ -1,9 +1,6 @@
 import axios from "axios"
 
-const isProd = process.env.NODE_ENV === 'production'
-
-export const server = isProd ? process.env.API_URL : 'http://localhost:8080'
-// export const server = dev ? 'http://localhost:8080' : 'http://ec2-18-141-208-106.ap-southeast-1.compute.amazonaws.com:8080'
+export const server = process.env.NEXT_PUBLIC_API_URL
 
 export const axiosInstance = axios.create({
     baseURL: `${server}/api/v1/`,
